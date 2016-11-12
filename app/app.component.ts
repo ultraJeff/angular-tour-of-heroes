8c17ae3e2833e029f7e0d6bf16f12772c9c6ead4
Jeffrey Franklin <jefrfranklin@gmail.com>
2016-11-11 22:03:21 -0800

    Hero string literal converted to a Hero class

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+
+// IT'S IMPORTANT THAT THIS CLASS IS ABOVE THE @COMPONENT METADATA!
+// CONSOLE ERROR OTHERWISE!
+export class Hero {
+	id: number;
+	name: string;
+}
+
 @Component({
   selector: 'my-app',
   template: `
   	<h1>{{title}}</h1>
-  	<h2>{{hero}} details!</h2>
+  	<h2>{{hero.name}} details!</h2>
 	`
 })
+
 export class AppComponent {
 	title = 'Tour of Heroes';
-	hero = 'Windstorm';
+	hero: Hero = {
+		id: 1,
+		name: 'Windstorm'
+	};
 }
