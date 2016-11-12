72cefe15a4ae6f7c1ab5a4e9a0ad0adcf6d100f2
Jeffrey Franklin <jefrfranklin@gmail.com>
2016-11-12 06:24:22 -0800

    Adding *ngIf to a parent DOM element fixes the console error by removing the view from the DOM entirely until triggered

--- app/app.component.ts
+++ app/app.component.ts
@@ -39,13 +39,15 @@ const HEROES: Hero[] = [
   			<span class="badge">{{hero.id}}</span> {{hero.name}}
 			</li>
 		</ul>
-  	<h2>{{selectedHero.name}} details!</h2>
-  	<div>
-  		<label class="ion-arrow-right-a">id: </label>{{selectedHero.id}}
-		</div>
-		<div>
-  		<label>name: </label>
-  		<input [(ngModel)]="selectedHero.name" placeholder="name">
+		<div *ngIf="selectedHero">
+	  	<h2>{{selectedHero.name}} details!</h2>
+	  	<div>
+	  		<label class="ion-arrow-right-a">id: </label>{{selectedHero.id}}
+			</div>
+			<div>
+	  		<label>name: </label>
+	  		<input [(ngModel)]="selectedHero.name" placeholder="name">
+			</div>
 		</div>
 	`
 })
