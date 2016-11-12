0ed9ebe1d8d4a4aa3680aa91322c19652000cd6d
Jeffrey Franklin <jefrfranklin@gmail.com>
2016-11-12 14:34:08 -0800

    Add simple routing between /heroes and /dashboard and redirect / to /dashboard

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,11 +3,20 @@ import { Component } from '@angular/core';
 import '../theme/fonts.css';
 import '../theme/global.scss';
 
+// The AppComponent is now attached to a router and displaying routed views.
+// For this reason and to distinguish it from other kinds of components, we call this type of component a Router Component.
+
 @Component({
 	selector: 'my-app',
 	template: `
 		<h1>{{title}}</h1>
-		<my-heroes></my-heroes>
+		<nav>
+			<!-- routerLink is preferred way to navigate with router -->
+			<!-- we define a routing instruction with a one-time binding to our route path -->
+			<a routerLink="/dashboard">Dashboard</a>
+			<a routerLink="/heroes">Heroes</a>
+		</nav>
+		<router-outlet></router-outlet>
 	`
 })
 
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,20 +1,45 @@
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule }      				from '@angular/core';
+import { BrowserModule } 				from '@angular/platform-browser';
+import { FormsModule } 					from '@angular/forms';
+import { RouterModule } 				from '@angular/router';
 
-import { AppComponent } from './app.component';
-import { HeroesComponent } from './heroes.component';
-import { HeroDetailComponent } from './hero-detail.component';
-import { HeroService } from './hero.service';
+import { AppComponent } 				from './app.component';
+import { DashboardComponent } 	from './dashboard.component';
+import { HeroesComponent } 			from './heroes.component';
+import { HeroDetailComponent } 	from './hero-detail.component';
+import { HeroService } 					from './hero.service';
 
 // If it starts with an @ - it's called a decorator... they're experimental!
 @NgModule({
   imports:      [
   	BrowserModule,
-  	FormsModule
+  	FormsModule,
+  	// The Angular router is an external, optional Angular NgModule called RouterModule.
+  	// The router is a combination of multiple provided services (RouterModule),
+  	// multiple directives (RouterOutlet, RouterLink, RouterLinkActive), and a configuration (Routes)
+  	// ========================================================================================================
+  	// We use the forRoot method because we're providing a configured router at the root of the application.
+  	// The forRoot method gives us the Router service providers and directives needed for routing, and performs
+  	// the initial navigation based on the current browser URL.
+  	RouterModule.forRoot([
+      {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+      },
+  		{
+  			path: 'heroes',
+  			component: HeroesComponent
+  		},
+  		{
+  			path: 'dashboard',
+  			component: DashboardComponent
+  		}
+  	])
 	],
   declarations: [
   	AppComponent,
+  	DashboardComponent,
   	HeroesComponent,
   	HeroDetailComponent
 	],
