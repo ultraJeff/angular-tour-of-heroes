d56498ba76d817d74c05adce855bd21040a1590b
Jeffrey Franklin <jefrfranklin@gmail.com>
2016-11-12 15:06:41 -0800

    Add Skeleton.css for quick CSS styling

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 import '../theme/fonts.css';
+import '../theme/vendor.css';
 import '../theme/global.scss';
 
 // The AppComponent is now attached to a router and displaying routed views.
@@ -13,8 +14,8 @@ import '../theme/global.scss';
 		<nav>
 			<!-- routerLink is preferred way to navigate with router -->
 			<!-- we define a routing instruction with a one-time binding to our route path -->
-			<a routerLink="/dashboard">Dashboard</a>
-			<a routerLink="/heroes">Heroes</a>
+			<a class="button button-primary" routerLink="/dashboard">Dashboard</a>
+			<a class="button button-primary" routerLink="/heroes">Heroes</a>
 		</nav>
 		<router-outlet></router-outlet>
 	`
