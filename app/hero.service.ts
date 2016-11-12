2189e46ae13e766c6ea856f27c337c2139090da8
Jeffrey Franklin <jefrfranklin@gmail.com>
2016-11-12 10:46:17 -0800

    Create hero service stub

---
+++ app/hero.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@angular/core';
+
+// We imported the Angular Injectable function and applied that function as an @Injectable() decorator.
+// Don't forget the parentheses!
+@Injectable()
+export class HeroService {
+	getHeroes(): void { } // stub
+}
