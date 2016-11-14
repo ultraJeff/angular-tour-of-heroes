a2e6919c48136c1f14b6e6133e101b75cc0b75fd
Jeffrey Franklin <jefrfranklin@gmail.com>
2016-11-14 11:50:22 -0800

    Add DELETE functionality and styled button

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -21,6 +21,13 @@ export class HeroService {
 			.then(res => res.json().data)
 			.catch(this.handleError);
 	}
+	delete(id: number): Promise<void> {
+		const url = `${this.heroesUrl}/${id}`;
+		return this.http.delete(url, {headers: this.headers})
+			.toPromise()
+			.then(() => null)
+			.catch(this.handleError);
+	}
 	getHero(id: number): Promise<Hero> {
 		return this.getHeroes()
 						.then(heroes => heroes.find(hero => hero.id === id));
